bb541dae10deb898a716191bb3372f7bd00646a7
sinkinben <sinkinben@outlook.com>
2022-05-12 16:48:13 +0800

    cs149 asst4: init serial page rank

--- cs149-asst4/pagerank/page_rank.cpp
+++ cs149-asst4/pagerank/page_rank.cpp
@@ -4,10 +4,10 @@
 #include <cmath>
 #include <omp.h>
 #include <utility>
+#include <algorithm>
 
 #include "../common/CycleTimer.h"
 #include "../common/graph.h"
-
 
 // pageRank --
 //
@@ -16,46 +16,67 @@
 // damping:     page-rank algorithm's damping parameter
 // convergence: page-rank algorithm's convergence threshold
 //
-void pageRank(Graph g, double* solution, double damping, double convergence)
+
+void pageRank(Graph g, double *solution, double damping, double convergence)
 {
 
+    // initialize vertex weights to uniform probability. Double
+    // precision scores are used to avoid underflow for large graphs
 
-  // initialize vertex weights to uniform probability. Double
-  // precision scores are used to avoid underflow for large graphs
+    int numNodes = num_nodes(g);
+    double equal_prob = 1.0 / numNodes;
+    Vertex *sink_nodes = new Vertex[numNodes];
+    int num_sinks = 0;
 
-  int numNodes = num_nodes(g);
-  double equal_prob = 1.0 / numNodes;
-  for (int i = 0; i < numNodes; ++i) {
-    solution[i] = equal_prob;
-  }
-  
-  
-  /*
-     CS149 students: Implement the page rank algorithm here.  You
-     are expected to parallelize the algorithm using openMP.  Your
-     solution may need to allocate (and free) temporary arrays.
+    /* collect all vertices with no outgoing edges, and
+     * init solution[i] with equal probability
+     */
+    for (int i = 0; i < numNodes; ++i)
+    {
+        solution[i] = equal_prob;
+        if (outgoing_size(g, i) == 0)
+            sink_nodes[num_sinks++] = i;
+    }
 
-     Basic page rank pseudocode is provided below to get you started:
 
-     // initialization: see example code above
-     score_old[vi] = 1/numNodes;
+    bool converged = false;
+    double *score_new = new double [numNodes];
+    double term = (1.0 - damping) / numNodes;
 
-     while (!converged) {
+    while (!converged)
+    {
+        /* traverse vertices with no outgoing edges */
+        double sink_val = 0;
+        for (int i = 0; i < num_sinks; ++i)
+        {
+            int u = sink_nodes[i];
+            sink_val += solution[u];
+        }
+        sink_val = sink_val * damping / numNodes;
 
-       // compute score_new[vi] for all nodes vi:
-       score_new[vi] = sum over all nodes vj reachable from incoming edges
-                          { score_old[vj] / number of edges leaving vj  }
-       score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
+        for (int v = 0; v < numNodes; ++v)
+        {
+            /* result of score_new[v] */
+            double val = 0.0;
 
-       score_new[vi] += sum over all nodes v in graph with no outgoing edges
-                          { damping * score_old[v] / numNodes }
+            /* traverse all edges of (u, v) */
+            const Vertex *end = incoming_end(g, v);
+            for (const Vertex *u = incoming_begin(g, v); u != end; ++u)
+                val += solution[*u] / outgoing_size(g, *u);
+            val = term + damping * val + sink_val;
 
-       // compute how much per-node scores have changed
-       // quit once algorithm has converged
+            score_new[v] = val;
+        }
 
-       global_diff = sum over all nodes vi { abs(score_new[vi] - score_old[vi]) };
-       converged = (global_diff < convergence)
-     }
+        double diff = 0.0;
+        for (int i = 0; i < numNodes; ++i)
+        {
+            diff += std::abs(score_new[i] - solution[i]);
+            solution[i] = score_new[i];
+        }
 
-   */
+        converged = (diff < convergence);
+    }
+    delete[] sink_nodes;
+    delete[] score_new;
 }
